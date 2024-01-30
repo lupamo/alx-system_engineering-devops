a7a00abccf9ace3742b72e66188013b16625127c
lupamo <arnoldlupamo@gmail.com>
2024-01-30 23:15:46 +0300

    b and t should be there 2

--- 0x06-regular_expressions/4-repetition_token_3.rb
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 #the string should only contain t and b in the middle
 
-repeat = /hb[t]*n/
+repeat = /hbt*n/
 
 matches = arg.scan(repeat)
 matches.each { |match| print match }
