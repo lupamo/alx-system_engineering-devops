8f17f816edf57028c1c8653b82c8cd48f544c8bc
lupamo <arnoldlupamo@gmail.com>
2024-03-22 17:19:25 +0300

    ensure error

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
 # puppet file creating a school file in tmp folder
+
 file {'/tmp/school':
-  sure    =>   'present',
+  ensure    =>   'present',
   mode    =>   '0744',
   owner   =>   'www-data',
   group   =>   'www-data',
-  content =>   'I love Puppet'
+  content =>   'I love Puppet',
 }
