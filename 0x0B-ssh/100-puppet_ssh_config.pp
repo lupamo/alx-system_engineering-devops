024e328b02910571324832ab8891f947f5ca264b
lupamo <arnoldlupamo@gmail.com>
2024-03-24 20:26:08 +0300

    puppet client config

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,14 @@
 # configuring ssh client using puppet
 
-file {'/etc/ssh/ssh_config':
-  ensure  => present,
-  content =>"
-  Host*
-  IdentifyFile ~/.ssh/school
-  PasswordAuthentication no"
+class file_config {
+  include stdlib
+  file_line {'Password auth turn off':
+  path => '/etc/ssh/sshd_config',
+  line => 'PasswordAuthentication no',
+}
+
+file_line {'checking file':
+  path => '/etc/ssh/ssh_config',
+  line => 'IdentityFile ~/.ssh/school',
+}
 }
