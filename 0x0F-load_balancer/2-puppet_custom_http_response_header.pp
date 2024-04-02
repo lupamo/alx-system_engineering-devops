59e886d65eff772e3de51c821351698b9e9d6078
lupamo <arnoldlupamo@gmail.com>
2024-04-02 03:34:20 +0300

    puppet to configure header

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,9 @@
+#automate the task of creating a custom HTTP header response
+
+exec {'cmd':
+  cmd      => 'apt-get -y update;
+	apt-get -y install nginx;
+	sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+	service nginx restart',
+  provider => shell,
+}
