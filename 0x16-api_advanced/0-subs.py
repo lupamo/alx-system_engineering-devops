1377ff331f478580f1ef80cb6661ef26e692f485
lupamo <arnoldlupamo@gmail.com>
2024-05-07 23:07:49 +0300

    subscriber api get

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Using reddit API"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    A function that returns the number of
+    reddit subscribers
+    """
+
+    if subreddit is not isinstance(subreddit, str):
+        return 0
+
+    headers = {'User-Agent': 'VICTORY'}
+    resp = requests.get(
+        'https://www.reddit.com/r/{}/about.json'.format(str(subreddit)),
+        headers=headers,
+        allow_redirects=False
+    )
+
+    if resp.status_code != 200:
+        return 0
+    data = resp.json()
+    return data.get('data').get('subscribers')
