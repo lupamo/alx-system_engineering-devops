dcd78665b074e24b997052523f2e43b3a6a925bd
lupamo <arnoldlupamo@gmail.com>
2024-05-07 23:45:39 +0300

    top ten host for a given subreddit

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Reddit api module call"""
+import requests
+
+
+def top_ten(subreddit):
+    """
+    A reddit function that ptints tittle
+    of the first ten host for a given subreddit
+    """
+    headers = {'User-Agent': 'VICTORY'}
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    resp = requests.get(url, headers=headers, allow_redirects=False)
+
+    if resp.status_code == 200:
+        data = resp.json()
+        for post in data['data']['children']:
+            print(post['data']['title'])
+    else:
+        print("None")
+
+
+if __name__ == '__main__':
+    import sys
+
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        top_ten(sys.argv[1])
