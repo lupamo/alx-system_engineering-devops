fd5755fa362fe1449652975adafe61460d7e75fe
lupamo <arnoldlupamo@gmail.com>
2024-06-05 05:27:33 +0300

    api function that prints tittle

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Reddit api module call"""
+import requests
+
+
+def top_ten(subreddit):
+    """
+    A reddit api function that prints tittle
+    of the first ten host for a given subreddit
+    """
+    headers = {'User-Agent': 'VICTORY'}
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json()
+        for post in data['data']['children']:
+            print(post['data']['title'])
+    else:
+        print("None")
+
+
+if __name__ == '__main__':
+    import sys
+
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        top_ten(sys.argv[1])
