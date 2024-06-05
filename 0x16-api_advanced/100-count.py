4e37cec0c0d16149f654945e677bf7f0250d1d52
lupamo <arnoldlupamo@gmail.com>
2024-06-05 05:38:33 +0300

    pycodestyle

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -4,8 +4,8 @@ import requests
 
 
 def count_words(subreddit, word_list, after=None, count=None):
-    """ recursive function that queries the Reddit API, 
-    parses the title of all hot articles, and prints 
+    """ recursive function that queries the Reddit API,
+    parses the title of all hot articles, and prints
     a sorted count of given keywords (case-insensitive,
     delimited by spaces
     """
