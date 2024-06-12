8a9e4296070c3650aa12fadc2a0fadc541ac157e
lupamo <arnoldlupamo@gmail.com>
2024-06-12 07:24:50 +0300

    requets

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,9 @@
+# how to adjust user request limit
+exec { 'nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin'
+}
+-> exec {'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+  }
